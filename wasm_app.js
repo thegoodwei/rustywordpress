82d1d5f250e7229b9e5c757ac544058a9f49d546
Wade <94668210+thegoodwei@users.noreply.github.com>
2023-01-13 13:39:09 -0600

    Update wasm_app.js
    Instantiate wasm from js

--- wasm_app.js
+++ wasm_app.js
@@ -1,20 +1,24 @@
 import init, { run_Wasmrs_App } from './wasm_app.wasm';
 
-async function run_Wasmjs_App() {
-    // Instantiate the WebAssembly module
-    const wasmModule = await init();
-
-    WebAssembly.instantiateStreaming(fetch('./wasm_module.wasm'))
-      .then(({module, instance}) => {
-         instance.exports.run_Wasmrs_App();
-     });
-
-    }
-
 document.addEventListener("DOMContentLoaded", function() {
     run_Wasmjs_App();
 });
 
+async function run_Wasmrs_App() {
+    // Instantiate the WebAssembly module
+    const wasmModule = await init();
+
+    WebAssembly.instantiateStreaming(fetch('./wasm_module.wasm'))
+    .then(({module, instance}) => {
+        if (instance.exports.run_Wasmrs_App) {
+            instance.exports.run_Wasmrs_App();
+        } else {
+            console.error("Error: run_Wasmrs_App() function not found in WebAssembly exports.");
+        }
+    }).catch(error => {
+        console.error(error);
+    });
+}
 
 
 //paste code from wasm-bindgen below
